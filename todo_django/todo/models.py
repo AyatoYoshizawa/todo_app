a1ce10c78a8bddaa5358b6f281c4a40f52ae9bdd
Ayato <ayatoyoshizawa9@gmail.com>
2024-04-26 22:41:23 +0900

    モデルを定義してマイグレーションを実行した。

--- todo_django/todo/models.py
+++ todo_django/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    status = models.IntegerField()
+    title = models.CharField(max_length=255)
+    deadline = models.DateTimeField()
